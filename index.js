fb8200d0961a7ff678164fd9729e70b938678588
maopos <mauricioposadamurillo@gmail.com>
2021-11-01 20:04:53 -0500

    borrando comentarios

--- index.js
+++ index.js
@@ -6,21 +6,21 @@ app.use(express.static(__dirname + '/public'))
 const PORT = process.env.PORT || 4000;
 
 app.get("/boos", (req, res) => {
-  //res.send("👷🏻‍♂️ Backend...");
+  
   res.json({
       mensaje: 'Hola desde boos'
   })
 });
 
 app.get("/project", (req, res) => {
-  //res.send("👷🏻‍♂️ Otra ruta...");
+  
   res.json({
       mensaje: 'Hola desde project'
   })
 });
 
 app.get("/master", (req, res) => {
-  //res.send("👷🏻‍♂️ Ruta master...");
+  
   res.json({
       mensaje: 'Hola desde master'
   })
